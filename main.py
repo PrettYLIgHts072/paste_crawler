8c9bed1194e5dc8b2f18691cb8228bd874d991e4
GeorgeKesaev <kesaev@gmail.com>
2019-10-11 00:12:18 +0300

    refactor and reordered crawler functions, cleaned logging

--- main.py
+++ main.py
@@ -1,51 +1,14 @@
-import asyncio
 import logging
-import random
 from typing import Optional, List
 
-import arrow
 import pymongo.collation
 import pymongo.errors
 import requests
 import yaml
 from lxml import html
-from pymongo import MongoClient
 from requests.exceptions import HTTPError
 
-
-def clean_trailing_spaces(in_str: str):
-    return '\n'.join(map(lambda line: line.strip(), in_str.split('\n')))
-
-
-def extract_features(page: bytes, xpath_rule: str) -> list:
-    tree = html.fromstring(page)
-    results_list = tree.xpath(xpath_rule)
-    return results_list
-
-
-def clean_untitled_unknown(text: [str, List[str]]) -> str:
-    try:
-        text = text[0]
-    except IndexError:
-        return ""
-    if text == 'Untitled' or text == 'Guest' or \
-            text == 'Unknown' or text == 'Anonymous':
-        return ""
-    return text
-
-
-async def random_wait(a: int = 0, b: int = 3, caller=None):
-    i = random.randint(a, b)
-    if caller:
-        logging.info(f"{caller} sleeping for {i} seconds.")
-    await asyncio.sleep(i)
-
-
-class Job:
-    def __init__(self, url: str, next_url, seed):
-        self.url = url
-        self.next_url = next_url
-        self.seed = seed
+from utils import *
 
 
 class Crawler:
@@ -57,8 +20,8 @@ class Crawler:
             print(f"settings file cant be fount at: {settings_path}")
 
         logging.basicConfig(format=self.config['logger_format'],
-                            level=logging.DEBUG,
-                            # level=logging.INFO,
+                            # level=logging.DEBUG,
+                            level=logging.INFO,
                             datefmt=self.config['logging_datefmt'])
         self.time_reformatter = TimeReformat(self.config['time_parse_str'],
                                              self.config['src_time_zone'],
@@ -66,109 +29,16 @@ class Crawler:
         self.to_run = True
         self.loop = asyncio.get_event_loop()
         self.url_queue = asyncio.Queue()
-        self.db_collection = self.get_db_collection(self.config['db'])
+        self.db = DB(self.config['db'])
         self.headers = self.config['browser_headers']
         self.schedulers = \
-            self.create_loop_event_tasks(self.produce, self.config['seeds'])
+            self.create_loop_event_tasks(self.scheduler, self.config['seeds'])
         self.downloaders = \
-            self.create_loop_event_tasks(self.download,
+            self.create_loop_event_tasks(self.downloader,
                                          range(self.config['num_connections']))
 
-    @staticmethod
-    def clean_results(time_reformatter, res: dict) -> dict:
-        return {'time': time_reformatter.reformat(res['time'][0]),
-                'title': clean_untitled_unknown(res['title']),
-                'author': clean_untitled_unknown(res['author']),
-                'content': clean_trailing_spaces(res['content'][0])}
-
     def create_loop_event_tasks(self, func, tasks):
-        rv = [self.loop.create_task(func(t)) for t in tasks]
-        return rv
-
-    async def fetch_page(self, url: str, retry: int = 2) -> Optional[bytes]:
-        try:
-            response = requests.get(url, headers=random.choice(self.headers))
-            response.raise_for_status()
-            return response.content
-        except HTTPError as err:
-            logging.error(f'http error occurred: {err} while getting: {url}')
-            if retry > 0:
-                await asyncio.sleep(120)
-                await self.fetch_page(url, retry - 1)
-            else:
-                raise Exception('Didn\'t get {} page'.format(url))
-
-    @staticmethod
-    def get_db_collection(conf):
-        mongo_client = MongoClient(conf['mongo_url'], conf['mongo_port'])
-        db = mongo_client[conf['db_name']]
-        return db['paste_collection']
-
-    def get_next_job(self, job):
-        return self.config['pages'][job['next_page']]
-
-    async def add_urls_to_crawl(self, job, content):
-        res = []
-        for feature, rule in job['features'].items():
-            res = extract_features(content, rule)
-        logging.debug(f"push links to queue {res[:5]}")
-        next_job = self.get_next_job(job)
-        for r in res:
-            next_job['url'] = str(r)
-            await self.url_queue.put(next_job.copy())
-
-    @staticmethod
-    async def get_paste_content(job, content):
-        return {feature: extract_features(content, rule)
-                for (feature, rule) in job['features'].items()}
-
-    def mark_job_done(self):
-        self.url_queue.task_done()
-
-    async def get_a_job(self):
-        return await self.url_queue.get()
-
-    async def download(self, name: int) -> None:
-        while True:
-            try:
-                await random_wait(caller=f"Consumer {name}")
-                job = await self.get_a_job()
-                logging.debug(f"Consumer {name} got element <{job}>"
-                              f" and {job['features']} {job['next_page']}")
-                job_url = job['seed'] + job['url']
-                content = await self.fetch_page(job_url)
-
-                if job['next_page']:
-                    await self.add_urls_to_crawl(job, content)
-                else:
-                    paste = await self.get_paste_content(job, content)
-                    paste = self.clean_results(self.time_reformatter, paste)
-                    paste['url'] = job_url
-                    self.save_result(paste)
-                self.mark_job_done()
-            except HTTPError as http_err:
-                logging.error(f'http error occurred: {http_err}')
-            except Exception as e:
-                logging.error(f"consumer got problem {e}")
-
-    def save_result(self, result):
-        try:
-            post_id = self.db_collection.update(
-                {'url': result['url']},
-                result,
-                upsert=True)
-            logging.debug(f"post successful, post id {post_id}")
-        except pymongo.errors.ServerSelectionTimeoutError as e:
-            logging.error("error saving results: {}".format(e))
-        except Exception as e:
-            logging.error("failed to save post to mongo", e)
-
-    async def produce(self, seed: dict) -> None:
-        while self.to_run:
-            await self.url_queue.put(self.config['pages'][seed['next_page']])
-            logging.info(f"Producer added "
-                         f"{{self.config['seeds'][0]['seed']}} to queue.")
-            await asyncio.sleep(seed['interval'])
+        return [self.loop.create_task(func(t)) for t in tasks]
 
     async def crawl(self) -> None:
         await asyncio.gather(*self.schedulers)
@@ -184,22 +54,106 @@ class Crawler:
     def stop(self):
         self.to_run = False
 
+    async def scheduler(self, seed: dict) -> None:
+        while self.to_run:
+            await self.url_queue.put(self.config['pages'][seed['next_page']])
+            logging.info(f"Producer added "
+                         f"{{self.config['seeds'][0]['seed']}} to queue.")
+            await asyncio.sleep(seed['interval'])
 
-class TimeReformat:
-    def __init__(self, parse_str, src_time_zone, res_format):
-        self.parse_str = parse_str
-        self.src_time_zone = src_time_zone
-        self.res_format = res_format
+    async def downloader(self, _id: int) -> None:
+        while True:
+            try:
+                await random_wait()
+                job = await self.get_a_job()
+                logging.info(f"Downloader {_id} got new job, features: "
+                             f" {job['features']}")
+                job_url = job['seed'] + job['url']
+                content = await self.page_content(job_url)
 
-    def reformat(self, in_time):
-        in_time = str(in_time)
+                if job['next_page']:
+                    await self.add_paste_urls_to_crawl(job, content)
+                else:
+                    paste = await self.get_paste_content(job, content)
+                    paste = self.format_content(paste)
+                    paste['url'] = job_url
+                    self.save_result(paste)
+                self.mark_job_done()
+            except HTTPError as http_err:
+                logging.error(f'http error occurred: {http_err}')
+            except Exception as e:
+                logging.error(f"consumer got problem {e}")
+
+    def format_content(self, res: dict) -> dict:
+        return {'time': self.time_reformatter.reformat(res['time'][0]),
+                'title': self.clean_untitled_unknown(res['title']),
+                'author': self.clean_untitled_unknown(res['author']),
+                'content': self.clean_trailing_spaces(res['content'][0])}
+
+    async def page_content(self, url: str, retry: int = 2) -> Optional[bytes]:
         try:
-            t = arrow.get(str(in_time), self.parse_str)
-            t = t.replace(tzinfo=self.src_time_zone)
-            t = t.to(self.res_format).ctime()
-            return t
+            response = requests.get(url, headers=random.choice(self.headers))
+            response.raise_for_status()
+            return response.content
+        except HTTPError as err:
+            logging.error(f'http error occurred: {err} while getting: {url}')
+            if retry > 0:
+                await asyncio.sleep(120)
+                await self.page_content(url, retry - 1)
+            else:
+                raise Exception('Didn\'t get {} page'.format(url))
+
+    def get_next_job(self, job):
+        return self.config['pages'][job['next_page']]
+
+    async def add_paste_urls_to_crawl(self, job, content):
+        paste_urls = [self.extract_features(content, rule)
+                      for rule in job['features'].values()][0]
+        logging.debug(f"push links to queue {paste_urls[:5]}")
+        next_job = self.get_next_job(job)
+        for r in paste_urls:
+            next_job['url'] = str(r)
+            await self.url_queue.put(next_job.copy())
+
+    async def get_paste_content(self, job, content):
+        return {feature: self.extract_features(content, rule)
+                for (feature, rule) in job['features'].items()}
+
+    @staticmethod
+    def extract_features(page: bytes, xpath_rule: str) -> list:
+        tree = html.fromstring(page)
+        results_list = tree.xpath(xpath_rule)
+        return results_list
+
+    @staticmethod
+    def clean_trailing_spaces(in_str: str):
+        return '\n'.join(map(lambda line: line.strip(), in_str.split('\n')))
+
+    @staticmethod
+    def clean_untitled_unknown(text: [str, List[str]]) -> str:
+        try:
+            text = text[0]
+        except IndexError:
+            return ""
+        if text == 'Untitled' or text == 'Guest' or \
+                text == 'Unknown' or text == 'Anonymous':
+            return ""
+        return text
+
+    async def get_a_job(self):
+        return await self.url_queue.get()
+
+    def mark_job_done(self):
+        self.url_queue.task_done()
+
+    def save_result(self, result):
+        try:
+            post_id = self.db.save(result)
+            logging.debug(f"Successfully saved, post id {post_id['upserted']}")
+        except pymongo.errors.ServerSelectionTimeoutError as e:
+            logging.error("error saving results: {}".format(e))
         except Exception as e:
-            logging.error("error formatting time {}".format(e))
+            logging.error("failed to save post to mongo", e)
 
 
 if __name__ == "__main__":
